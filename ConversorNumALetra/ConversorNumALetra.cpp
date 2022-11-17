1f433839574a18da4c1b3af4c3fb75c680299e25
WilbertLeon <1105634@est.intec.edu.do>
2022-11-17 17:22:33 -0400

    centenas a palabras y documentacion

--- ConversorNumALetra/ConversorNumALetra.cpp
+++ ConversorNumALetra/ConversorNumALetra.cpp
@@ -1,8 +1,29 @@
+/*
+ Asignatura: IDS344-01 - ESTRUCTURAS DE DATOS Y ALGORITMOS II
+
+ Docente: CASIMIRO GILBERTO CORDERO RAMIREZ
+
+ Estudiantes:
+
+ - Jean Güichardo 1105491
+ - Ricardo Acosta 1104082
+ - Wilbert León 1105634
+ - Sebastian Peralta 1105487
+ - Jesus Bido 1105909
+
+ Evidencia: Convertir número a palabra
+
+ Descripcion: Realizar un programa C++ que lea un número real (correspondiente a un monto) y convertir este monto a palabra.
+
+ Fecha: 16/11/2022
+*/
+
 #include <iostream>
 #include <string>
 using namespace std;
 
 string Convierte(int n, int i);
+string ConvierteCent(int n);
 void SeparaGruposDigitos(string d, int gd[]);
 bool Validar(string c);
 
@@ -12,7 +33,7 @@ int main(int argc, const char* argv[]) {
 	int dg[10] = { 0,-1,-1,-1,-1,-1,-1,-1,-1,-1 };
 	bool validar = true;
 	string dig = "", c;
-	string num = "", k = "", copia;
+	string num = "", k = "", copia, centavos;
 	string deno[8] = { "","mil","millones","millardos","billones","mil billones","trillones","mil trillones" };
 
 	do
@@ -20,7 +41,7 @@ int main(int argc, const char* argv[]) {
 		system("cls");
 		cout << "Introduzca la cantidad a convertir a Palabra: ";
 		cin >> k;
-		validar = Validar(c);  //Verifica si se ingresó un número
+		validar = Validar(k);  //Verifica si se ingresó un número
 
 	} while (validar == false);  //Repite mientras el valor no sea un número
 
@@ -28,13 +49,15 @@ int main(int argc, const char* argv[]) {
 
 	while (dg[i] != -1) {
 		copia = Convierte(dg[i], i); // retorna un numero de 3 digitos en palabras
-		if (copia == "uno" && i > 1) copia = "un";
+		if (copia == "uno" && i >= 1) copia = "un";
 		if (copia == "un" && i == 2) copia = "";
-		(copia == "" && dg[3] == 1) ? num = copia + " " + num : num = copia + " " + deno[i - 1] + " " + num;
+		(copia == "" && dg[2] != 1) ? num = copia + " " + num : num = copia + " " + deno[i - 1] + " " + num;
 		i++;
 	}
 
-	cout << num << "pesos con " << dg[0] << " centavos" << endl;
+	centavos = ConvierteCent(dg[0]);
+
+	cout << num << "pesos con " << centavos << " centavos" << endl;
 	return 0;
 }
 
@@ -50,6 +73,22 @@ string Convierte(int n, int ciclo)
 	cadena = cadena + palabra[pos + c]; //se toma la palabra correspondiente a las centenas
 	if (c == 1 && r > 0) cadena = cadena + "to"; // ciento en el caso de que sea > 100
 	d = d / 10; n = r; pos = 9; // posicion de las decenas
+	c = n / d; r = n % d; // digitos de las decenas y unidad
+	if (c == 1 && (r >= 1 && r <= 5)) { cadena = cadena + " " + palabra[pos * (c + 1) + r]; c = 0; r = 0; } // casi de los numerales especiales once al quince
+	if (c == 1 && r == 0) cadena = cadena + " " + palabra[pos + c]; // primera decena sin residuo (diez)
+	if (c == 1 && r > 5) cadena = cadena + " dieci"; // agregar palabra dieci a numerales
+	else if (c > 1) cadena = cadena + " " + palabra[pos + c]; // agregar palabra de la decena
+	n = r; pos = 24; // agregan las unidades
+	(c > 1 && r > 0) ? cadena = cadena + " y " + palabra[pos + n] : cadena = cadena + palabra[pos + n];
+	return cadena;
+}
+
+string ConvierteCent(int n)
+{
+	string palabra[34] = { "","cien","doscientos ","trescientos ","cuatrocientos ","quinientos ","seiscientos","setecientos ","ochocientos ","novecientos","diez","veinte","treinta","cuarenta","cincuenta","sesenta","setenta","ochenta","noventa","once","doce","trece","catorce","quince","","uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve" };
+	string cadena = "";
+	int c, r, d = 100, pos = 0;
+	d = d / 10; pos = 9; // posicion de las decenas
 	c = n / d; r = n % d; // digitos de las decenas y unidad
 	if (c == 1 && (r >= 1 && r <= 5)) { cadena = cadena + " " + palabra[pos * (c + 1) + r]; c = 0; r = 0; } // casi de los numerales especiales once al quince
 	if (c == 1 && r == 0) cadena = cadena + " " + palabra[pos + c]; // primera decena sin residuo (diez)
