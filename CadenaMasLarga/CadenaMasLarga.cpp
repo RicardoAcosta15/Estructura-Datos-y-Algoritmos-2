d2c41a5a527cdd95486ff994dcc4ad9089444c82
RicardoAcosta15 <1104082@est.intec.edu.do>
2022-11-24 00:15:24 -0400

    Documentacion

--- CadenaMasLarga/CadenaMasLarga.cpp
+++ CadenaMasLarga/CadenaMasLarga.cpp
@@ -1,26 +1,41 @@
-// CadenaMasLarga.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+ Asignatura: IDS344-01 - ESTRUCTURAS DE DATOS Y ALGORITMOS II
+
+ Docente: CASIMIRO GILBERTO CORDERO RAMIREZ
+
+ Estudiantes:
+
+ - Jean Güichardo 1105491
+ - Ricardo Acosta 1104082
+ - Wilbert León 1105634
+ - Sebastian Peralta 1105487
+ - Jesus Bido 1105909
+
+ Evidencia: Cadena Más Larga
+
+ Fecha: 24/11/2022
+*/
 
 #include <iostream>
 
 using namespace std;
 
+//Prototipo de funciones
 void CreacionCadenas(int i);
-void CadenaMasLarga(int i);
+void CadenaMasLarga(int a);
 
 int Cont[98], Max, Pos = 0;
 
 int main()
 {
-
-	for (int i = 1; i < 100; i++)
+	for (int i = 1; i < 100; i++) // Se evalúan los num del 1 al 99
 	{
 		CreacionCadenas(i);
 	}
 
-	Max = Cont[0];
+	Max = Cont[0];  // Max toma la primera pos del array
 
-	for (int a = 0; a < 99; a++)
+	for (int a = 0; a < 99; a++)  //Explícame ahí w pls
 	{
 		if (Cont[a] > Max)
 		{
@@ -35,43 +50,41 @@ int main()
 	return 0;
 }
 
-void CreacionCadenas(int i)
+void CreacionCadenas(int i) // Creamos las cadenas
 {
-	int N = 0, C = 0, M;
-	bool Conf = true;
+	int num = 0, cont = 0, mod;
+	bool confirmar = false;
 
-	Conf = true;
-	N = i;
-	C = 0;
+	num = i;
 
-	while (Conf != false)
+	while (confirmar != true)
 	{
-		M = N % 2;
+		mod = num % 2; // Usamos el módulo para saber si es par
 
-		(M == 0) ? N = (N / 2) : N = (N * 3) + 1;
+		(mod == 0) ? num /= 2 : num = (num * 3) + 1;  // Si el mod es 0 significa que el num es par
 
-		if (N == 4) { Conf = false; }
+		if (num == 4) { confirmar = true; }  // Si num llega a 4 paramos la cadena
 
-		C++;
+		cont++;  // Cuenta las repeticiones
 	}
-	Cont[i - 1] = C;
+	Cont[i - 1] = cont;  //Guarda el total de repeticiones del num en un array
 }
 
-void CadenaMasLarga(int a)
+void CadenaMasLarga(int a)  // Determina la cadena más larga
 {
-	int N = 0, M;
-	bool Conf = true;
-	N = a;
-	cout << N << " = ";
+	int num = 0, mod;
+	bool confirmar = false;
+	num = a;
+	cout << num << " = ";
 
-	while (Conf != false)
+	while (confirmar != true)
 	{
-		M = N % 2;
+		mod = num % 2;
 
-		(M == 0) ? N = (N / 2) : N = (N * 3) + 1;
+		(mod == 0) ? num /= 2 : num = (num * 3) + 1;
 
-		(N != 4) ? cout << N << " - " : cout << N << "\n" << endl;
+		(num != 4) ? cout << num << " - " : cout << num << "\n" << endl; //Mientras num no sea 4, imprime un guión
 
-		if (N == 4) { Conf = false; }
+		if (num == 4) { confirmar = true; }
 	}
 }
